7196bff59fa15b19e6a046cad36af80428442eac
Lucas <lucas.araujo.pereira@gmail.com>
2020-10-08 10:42:28 -0300

    testing remove multi

--- server/index.js
+++ server/index.js
@@ -64,20 +64,20 @@ console.log(tableToArray(table))
 }
 
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+// // Multi-process to utilize all CPU cores.
+// if (!isDev && cluster.isMaster) {
+//   console.error(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+//   // Fork workers.
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+//   });
 
-} else {
+// } else {
   const app = express();
 
   // Priority serve any static files.
@@ -102,4 +102,4 @@ if (!isDev && cluster.isMaster) {
   app.listen(PORT, function () {
     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
   });
-}
+// }
